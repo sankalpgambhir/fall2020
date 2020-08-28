19183cef79a1241c173158390cca7a0bc6579160
parthsastry <70089292+parthsastry@users.noreply.github.com>
2020-08-28 12:19:29 +0530

    Update 4-7.py

--- cs433/assignments/lec4/4-7.py
+++ cs433/assignments/lec4/4-7.py
@@ -11,3 +11,8 @@ f = Function('f', u , u ) # declaring a function of the sort
 P = Function('P', u , BoolSort())
 phi = And(f(c) == c, P(f(c)))
 solve(phi)
+
+print('c comes out to be U!Val!0, a constant sort.') 
+print('The function f(x) returns c for x = c, and returns c for x != c, implying it is a constant function.')
+print('The Predicate P(x) returns true for x = c and True for x != c, implying it is a constant predicate that always evaluates to True.')
+print('These assignments solve our phi')
