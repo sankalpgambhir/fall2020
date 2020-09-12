16284dc05788763f13f9d47a157aa8ee3fddaebc
Sankalp Gambhir <sankalp.gambhir47@gmail.com>
2020-09-12 13:08:51 +0530

    Add 433 as1

--- cs433/assignments/as1/main.py
+++ cs433/assignments/as1/main.py
@@ -1,7 +1,41 @@
-import expr_vector
+#!/bin/python3
 
-def main():
-    return
+from sympy import *
+from sympy.parsing.sympy_parser import parse_expr, standard_transformations, implicit_multiplication
+import sys
+
+def is_expr_linear(expr, vars):
+    for x in vars:
+        for y in vars:
+            try: 
+                if not Eq(diff(expr, x, y), 0):
+                    return False
+            except TypeError:
+                return False
+    return True
+
+def main(inp):
+    try:
+        transformations = standard_transformations + (implicit_multiplication,)
+        expr = parse_expr(inp, transformations=transformations)
+    except:
+        print('Parse error!')
+        return None
+    
+    var = list(expr.free_symbols)
+
+    if not is_expr_linear(expr, var):
+        return False
+    elif expr.is_constant():
+        return [expr]
+    else:
+        ret = list(reversed((Poly(expr).coeffs())))
+        return ret
 
 if __name__ == '__main__':
-    main()
+    res = main(sys.argv[1])
+
+    if res is False:
+        print('Non linear function >:/')
+    else:
+        print(res)
---
+++ cs433/assignments/as1/test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "x − 2*x + y + 4"
+./main.py "x-2*x+y+4"
+echo "x − x + 4*y − 2 * (2 * y)"
+./main.py "x - x + 4*y - 2*(2*y)"
+echo "(x + 1) ∗ y"
+./main.py "(x+1)*y"
