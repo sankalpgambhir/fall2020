8807363435b15b59ef4e805d045de11f0b972c21
Sankalp Gambhir <sankalp.gambhir47@gmail.com>
2020-10-11 22:12:32 +0530

    Add skeleton

---
+++ cs433/assignments/as3/decisiontree.py
@@ -0,0 +1,14 @@
+import z3
+import decision
+
+class decision_tree:
+    def __init__(self, size, l = None, r = None):
+
+        assert (l == None) or (isinstance(l, decision_tree)), "Invalid data type passed for l!" 
+        assert (r == None) or (isinstance(r, decision_tree)), "Invalid data type passed for r!"
+        assert (isinstance(size, int)) and (size > 0), "Invalid decision size!" 
+
+        self.left = l;
+        self.right = r;
+
+        pass
---
+++ cs433/assignments/as3/main.py
@@ -0,0 +1,5 @@
+import decisiontree
+
+if __name__ == '__main__':
+    # do things
+    pass
