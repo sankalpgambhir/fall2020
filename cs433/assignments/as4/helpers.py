9c702782e4643ead5064ade218f56bc6a51ca8c7
Parth Sastry <70089292+parthsastry@users.noreply.github.com>
2020-12-10 15:05:17 +0530

    Update helpers.py

--- cs433/assignments/as4/helpers.py
+++ cs433/assignments/as4/helpers.py
@@ -19,11 +19,11 @@ def lintrans(W, v):
 
 def constrain_input(inp_z3, inp_real, epsilon):
     assert (len(inp_z3) == len(inp_real)), "Incompatible input vectors for constraints"
-    assert (epsilon >= 0), "Negative perturbation provided! Unbounded constraints"
+    assert (epsilon >= 0), "Negative perturbation provided! Bounded constraints"
     if(epsilon == 0):
         return z3.And([inp_z3[i] == inp_real[i] for i in range(len(inp_z3))])
     else:
-        return z3.And([z3.And((inp_z3[i] < inp_real[i] + epsilon), (inp_z3[i] > inp_real[i] - epsilon)) for i in range(len(inp_z3))])
+        return z3.And([z3.Or((inp_z3[i] > inp_real[i] + epsilon), (inp_z3[i] < inp_real[i] - epsilon)) for i in range(len(inp_z3))])
     
     assert 0, "Invalid flow of control"
     pass
